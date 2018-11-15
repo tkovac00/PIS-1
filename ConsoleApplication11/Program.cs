d4a23a31f7bc36e78412e5c78444e8a98d033634
Rea23 <36733245+Rea23@users.noreply.github.com>
2018-11-15 23:38:48 +0100

    vjezba za Git Desktop

--- ConsoleApplication11/Program.cs
+++ ConsoleApplication11/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            // sta ima
+            Console.WriteLine("promjena");
             //vjezba
         }
     }
